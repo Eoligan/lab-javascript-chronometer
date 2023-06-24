634e267f709227bfdfe7230320430e178d0cb448
Eoligan <sergio.hoyo.45@gmail.com>
2023-06-24 10:10:36 +0200

    logic

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,41 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      console.log(this.currentTime);
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ('0' + value).slice(-2);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    console.log('detenido');
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
 
@@ -37,3 +44,9 @@ class Chronometer {
 if (typeof module !== 'undefined') {
   module.exports = Chronometer;
 }
+
+const chrono = new Chronometer();
+chrono.currentTime = 65;
+console.log(chrono.computeTwoDigitNumber(chrono.getMinutes()));
+console.log(chrono.computeTwoDigitNumber(chrono.getSeconds()));
+console.log(chrono.split());
